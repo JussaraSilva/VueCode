e93dbf191237a6240e347e96e2d4f037d47f4a66
Jussara Cristina <jujujussara2013@gmail.com>
2025-07-24 14:51:06 -0300

    Estou trabalhando na página do editor, já adicionei a função de code com a biblioteca do CodeMirror, agora preciso trabalhar no iframe para visualizar o resultado

---
+++ js/editor.js
@@ -0,0 +1,9 @@
+document.getElementById('toggle-explorador').addEventListener('click', () => {
+  const painel = document.getElementById('painel-explorador');
+  painel.classList.toggle('oculto');
+});
+
+
+
+
+
